0f13eea9c46885be6b542120ac109ec53dceafb8
Nicolai Andersen <n.andersen758@gmail.com>
2018-05-18 16:41:11 +0200

    Repository for TransactionWindow added with connection string and database name. The connect funktion is implemented to connect on constructor call. Controller for TransactionWindowAdded

---
+++ TradeInfoDB/Controllers/TransactionWindowController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TradeInfoDB.Models;
+using TradeInfoDB.Repositories;
+
+namespace TradeInfoDB.Controllers
+{
+    public class TransactionWindowController : ApiController
+    {
+        private readonly TransactionWindowRepository _repository = new TransactionWindowRepository();
+
+        // GET: api/TransactionWindow
+        public IEnumerable<TransactionWindow> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        // GET: api/TransactionWindow/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TransactionWindow
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TransactionWindow/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TransactionWindow/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TradeInfoDB/Repositories/TransactionWindowRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using TradeInfoDB.Models;
+
+namespace TradeInfoDB.Repositories
+{
+    public class TransactionWindowRepository
+    {
+        private DocumentClient _client;
+        private const string _endPointURL = "https://localhost:8081";
+        private const string _primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        private const string databaseName = "TradeInfoDB";
+        private const string collectionName = "TransactionWindowCollection";
+
+        public TransactionWindowRepository()
+        {
+            var connectTask = Connect();
+            Task.WaitAll(connectTask);
+        }
+
+        public IEnumerable<TransactionWindow> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+        public async Task Connect()
+        {
+            try
+            {
+                _client = new DocumentClient(new Uri(_endPointURL), _primaryKey);
+                _client.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseName }).Wait();
+                await _client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseName),
+                    new DocumentCollection {Id = collectionName});
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Debug.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Debug.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Debug.WriteLine("Connection == true");
+            }
+        }
+
+        
+    }
+}
